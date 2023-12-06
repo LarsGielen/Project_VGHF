4627a7f40ccc40f0f0f1b8308083c05d8acf0fc5
LarsG-2158052 <lars.gielen@student.uhasselt.be>
2023-12-06 04:08:50 +0100

    Update models and views

--- src/main/kotlin/be/dbproject/models/ItemGenreLink.kt
+++ src/main/kotlin/be/dbproject/models/ItemGenreLink.kt
@@ -1,12 +1,23 @@
 package be.dbproject.models
 
-import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.Table
 
 @Entity
 @Table(name = "ItemGenreLink")
 data class ItemGenreLink(
-    @Column
-    val genreId: Int
+    @ManyToOne
+    @JoinColumn(name = "itemid")
+    var item: Item,
+
+    @ManyToOne
+    @JoinColumn(name = "genreid")
+    var genre: Genre
 ) : DataBaseModel()
+{
+    override fun toString(): String {
+        return "$item -> $genre"
+    }
+}
